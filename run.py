64ebdf03d34431a33404df9043fbb5b58fbaa851
Mister Navalha <felipe.saraiva@gmail.com>
2025-12-14 15:40:40 -0300

    Adiciona configuraÃ§Ãµes para deploy no Railway

--- run.py
+++ run.py
@@ -2,15 +2,17 @@
 """
 Script principal para executar a aplicação.
 """
+import os
 import uvicorn
 from app.config import get_settings
 
 if __name__ == "__main__":
     settings = get_settings()
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run(
         "app.api.main:app",
         host="0.0.0.0",
-        port=8000,
+        port=port,
         reload=settings.app_env == "development"
     )
 
